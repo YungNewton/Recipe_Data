840dfb6c23159404686f1c1228e300d94ccf311a
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-07-07 14:39:24 +0100

    commit

--- connector.py
+++ connector.py
@@ -1,14 +1,2 @@
-import pandas as pd
-from sqlalchemy import create_engine
+import MySQLdb
 
-# read data from csv file
-data = pd.read_csv('/path/to/your/file.csv')
-
-# create sqlalchemy engine
-engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
-                       .format(user="your_username",
-                               pw="your_password",
-                               db="your_database"))
-
-# insert data into table
-data.to_sql('Users', con = engine, if_exists = 'append', chunksize = 1000, index=False)
