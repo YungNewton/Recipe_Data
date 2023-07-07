405267223f99a894704c65c57134c9411fadabe9
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-07-07 14:54:34 +0100

    commit

--- connector.py
+++ connector.py
@@ -1,2 +1,15 @@
-import MySQLdb
+import mysql.connector
+
+# Create a connection
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="Dbeastinme1#",
+  database="recipe"  # Include this line if you want to connect to a specific database
+)
+
+# Create a cursor
+mycursor = mydb.cursor()
+
+# Now you can use mycursor to execute commands
 
