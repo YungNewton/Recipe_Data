badbda31f7323545966b14a9e4a2e62b3eb325d1
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-07-08 20:07:46 +0100

    commit

--- homework.py
+++ homework.py
@@ -6,7 +6,8 @@ mydb = sq.connect(
   host="localhost",
   user="root",
   password="Dbeastinme1#",
-  database="nj_state_teachers_salaries"  # Include this line if you want to connect to a specific database
+  database="nj_state_teachers_salaries",  # Include this line if you want to connect to a specific database
+  allow_local_infile=True
 )
 
 if mydb.is_connected():
@@ -14,7 +15,7 @@ if mydb.is_connected():
  #Create a cursor
 mycursor = mydb.cursor()
 # Now you can use mycursor to execute commands
-#mycursor.execute("CREATE DATABASE IF NOT EXISTS nj_state_teachers_salaries")
+mycursor.execute("CREATE DATABASE IF NOT EXISTS nj_state_teachers_salaries")
 mycursor.execute("""
 CREATE TABLE teachers_salaries (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -22,3 +23,15 @@ CREATE TABLE teachers_salaries (
     salary VARCHAR(255)
 )
 """)
+query = """
+LOAD DATA LOCAL INFILE '/Users/decagon/Downloads/cs101-pset3-1/nj_teachers_salaries_pset3.csv'
+INTO TABLE teachers_salaries
+FIELDS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(teacher_name, salary)
+"""
+
+mycursor.execute(query)
+mydb.commit()
