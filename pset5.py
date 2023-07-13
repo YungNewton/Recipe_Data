b058e44eff06b7814d5aa66a750ff840a4172b5d
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-07-14 00:25:29 +0100

    commit

--- pset5.py
+++ pset5.py
@@ -3,13 +3,13 @@ import pandas as pd
 import numpy as np
 
 # read QCTest.csv into df_test
-df_test = pd.read_csv('QCTest.csv')
+df_test = pd.read_csv('/Users/decagon/Downloads/cs101-pset5/QCTest.csv')
 
 # read specific columns of zip_code_database.csv into df_zip
-df_zip = pd.read_csv('zip_code_database.csv', usecols=['Zip Code', 'Primary City', 'STATE', 'IRS Estimated population'])
+df_zip = pd.read_csv('/Users/decagon/Downloads/cs101-pset5/zip_code_database.csv', usecols=['Zip Code', 'Primary City', 'STATE', 'IRS Estimated population'])
 
 # read defect_returns_by_state.csv into df_defects
-df_defects = pd.read_csv('defect_returns_by_state.csv')
+df_defects = pd.read_csv('/Users/decagon/Downloads/cs101-pset5/defect_returns_by_state.csv')
 
 # display the first few rows of df_test
 print("First few rows of df_test:")
@@ -23,24 +23,24 @@ print(df_zip.head())
 print("\nFirst few rows of df_defects:")
 print(df_defects.head())
 
-def CleanColumnHeading(df):
-    # Convert column names to lower case
-    df.columns = df.columns.str.lower()
+# def CleanColumnHeading(df):
+#     # Convert column names to lower case
+#     df.columns = df.columns.str.lower()
     
-    # Replace spaces with underscore
-    df.columns = df.columns.str.replace(' ', '_')
+#     # Replace spaces with underscore
+#     df.columns = df.columns.str.replace(' ', '_')
     
-    return df
+#     return df
 
-# Apply the CleanColumnHeading function to each dataframe
-df_test = CleanColumnHeading(df_test)
-df_zip = CleanColumnHeading(df_zip)
-df_defects = CleanColumnHeading(df_defects)
+# # Apply the CleanColumnHeading function to each dataframe
+# df_test = CleanColumnHeading(df_test)
+# df_zip = CleanColumnHeading(df_zip)
+# df_defects = CleanColumnHeading(df_defects)
 
-# Rename specific columns in df_zip
-df_zip.rename(columns={'zip_code': 'zip', 'primary_city': 'city'}, inplace=True)
+# # Rename specific columns in df_zip
+# df_zip.rename(columns={'zip_code': 'zip', 'primary_city': 'city'}, inplace=True)
 
-# Print out column names for each dataframe
-print("df_test columns:", df_test.columns)
-print("df_zip columns:", df_zip.columns)
-print("df_defects columns:", df_defects.columns)
+# # Print out column names for each dataframe
+# print("df_test columns:", df_test.columns)
+# print("df_zip columns:", df_zip.columns)
+# print("df_defects columns:", df_defects.columns)
