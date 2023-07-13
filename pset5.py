17c33e01f1528dfc82e8407ede006ccb7fce9513
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-07-13 23:22:47 +0100

    commit

--- pset5.py
+++ pset5.py
@@ -22,3 +22,25 @@ print(df_zip.head())
 # display the first few rows of df_defects
 print("\nFirst few rows of df_defects:")
 print(df_defects.head())
+
+def CleanColumnHeading(df):
+    # Convert column names to lower case
+    df.columns = df.columns.str.lower()
+    
+    # Replace spaces with underscore
+    df.columns = df.columns.str.replace(' ', '_')
+    
+    return df
+
+# Apply the CleanColumnHeading function to each dataframe
+df_test = CleanColumnHeading(df_test)
+df_zip = CleanColumnHeading(df_zip)
+df_defects = CleanColumnHeading(df_defects)
+
+# Rename specific columns in df_zip
+df_zip.rename(columns={'zip_code': 'zip', 'primary_city': 'city'}, inplace=True)
+
+# Print out column names for each dataframe
+print("df_test columns:", df_test.columns)
+print("df_zip columns:", df_zip.columns)
+print("df_defects columns:", df_defects.columns)
