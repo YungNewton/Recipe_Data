40daab289a9d62946bf383ca22067bae80f995e4
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-07-19 23:39:27 +0100

    commit

--- finalSoln.py
+++ finalSoln.py
@@ -3,3 +3,9 @@
 # In the given code, the issue arises from the way Python handles mutable data types like lists. When you pass the list as an argument to a function in Python, it is passed by reference, not by value.
 # This means that if the list is modified within the function, the changes will persist outside of it, which is not always the intended behavior.
 
+def unpairAtMost(n: int, pairs: List[Tuple[int, int]]) -> List[int]:
+    if n <= 0:
+        return []
+    else:
+        pairs[:n] = [num for pair in pairs[:n] for num in pair]
+        return pairs[:2*n]
