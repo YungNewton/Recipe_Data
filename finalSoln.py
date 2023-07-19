36835cd6863c5ca548e5e527bc3c6832045669e2
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-07-19 23:42:01 +0100

    commit

--- finalSoln.py
+++ finalSoln.py
@@ -1,7 +1,10 @@
+from typing import Callable, List, Tuple
+
 # Item 3a:
 
 # In the given code, the issue arises from the way Python handles mutable data types like lists. When you pass the list as an argument to a function in Python, it is passed by reference, not by value.
 # This means that if the list is modified within the function, the changes will persist outside of it, which is not always the intended behavior.
+# Consider a correct implementation of unpair_at_most as below:
 
 def unpairAtMost(n: int, pairs: List[Tuple[int, int]]) -> List[int]:
     if n <= 0:
@@ -9,3 +12,7 @@ def unpairAtMost(n: int, pairs: List[Tuple[int, int]]) -> List[int]:
     else:
         pairs[:n] = [num for pair in pairs[:n] for num in pair]
         return pairs[:2*n]
+#Let's consider the test case (2, [(10,20),(30,40),(50,60)]). 
+# The unpair_at_most function first changes the pairs list in-place to [10, 20, 30, 40, (50, 60)], and then it returns the first 2*n elements, which are [10, 20, 30, 40]. 
+# The problem is, when the model_solution is called next with the same test case, the pairs list is already modified.
+# So, the result of model_solution would be different from unpair_at_most, causing checker to return False, even though unpairAtMost is actually correct.
