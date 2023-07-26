a6339d129ead10926eeec7e332676054507044d4
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-07-26 16:32:23 +0100

    commit

--- obj.py
+++ obj.py
@@ -1,3 +1,13 @@
 from CarFacts import Car
 
-new_car = Car()
+new_car = Car('black',4,'ferrari')
+
+import pandas as pd
+midterm_scores = [96,90,85]
+df = pd.DataFrame(midterm_scores, columns=['midterm_scores'])
+
+print(type(df))
+
+shape = df.shape
+print(shape)
+df.sort_values(by='midterm_scores', ascending=False)
