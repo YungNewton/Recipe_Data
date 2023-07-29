227cf38017c9db077d5a16bad010264e1c5ca1c8
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-07-29 01:36:03 +0100

    commit

--- pset7.py
+++ pset7.py
@@ -4,4 +4,10 @@ class Account:
         self.u_name = u_name
 
 class Order(Account):
-    def __init__ (self, midterm, password, meal_cost)
+    def __init__ (self, cust_num, u_name, midterm, password, meal_cost, balance):
+        super().__init__(cust_num, u_name)
+        self.__midterm = midterm
+        self.__password = password
+        self.meal_cost = meal_cost
+        self.balance = balance
+    
