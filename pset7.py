1d48b5808095eb8a84786fc0777a54b131354ccb
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-07-29 02:09:51 +0100

    commit

--- pset7.py
+++ pset7.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Account:
     def __init__ (self, cust_num, u_name):
         self.cust_num = cust_num
@@ -19,4 +22,20 @@ class Order(Account):
         return self.__password
     def set_midterm(self, score):
         self.__midterm = score
-    def update_balance()
+    def update_balance(self):
+        if self.meal_cost == '0':
+            self.balance = 0
+        else:
+            points = 0
+            extraPoints = 5 * (math.floor((self.meal_cost - 15) / 15))
+            if self.meal_cost >= 15:
+                points += 10
+                points += extraPoints
+                if self.u_name == "HARVARD":
+                    points += 5
+                    if self.__midterm >= 80:
+                        points += 3
+                elif self.u_name == "MIT":
+                    points += 3
+                self.balance += points
+
