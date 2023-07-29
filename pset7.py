f6df2f3fda792a90da47418db1fa0fcb4804df3d
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-07-29 02:40:05 +0100

    commit

--- pset7.py
+++ pset7.py
@@ -7,7 +7,7 @@ class Account:
         self.u_name = u_name
 
 class Order(Account):
-    def __init__ (self, cust_num, u_name, midterm, password, meal_cost = 0, balance = 0):
+    def __init__ (self, cust_num, u_name, midterm , meal_cost, password , balance = 0):
         super().__init__(cust_num, u_name)
         self.__midterm = midterm
         self.__password = password
@@ -23,7 +23,7 @@ class Order(Account):
     def set_midterm(self, score):
         self.__midterm = score
     def update_balance(self):
-        if self.meal_cost == '0':
+        if self.__password == '0':
             self.balance = 0
         else:
             points = 0
@@ -39,3 +39,15 @@ class Order(Account):
                     points += 3
                 self.balance += points
 
+
+import numpy as np
+test3 = (1002, 'MIT', 89, 0, "9879")
+meal_costs = np.arange(0, 300, 100) 
+
+order = Order(test3[0],test3[1],test3[2],test3[3],test3[4])
+order.update_balance()
+
+for meal_cost in meal_costs:
+    order.meal_cost = meal_cost
+    order.update_balance()
+    print(f"Balance with meal cost {order.meal_cost} is: {order.get_balance()}")
